6e266e8dc7702fcf6a13e5a573b9fd56cb4b98fc
wanjaeliud <eliudwys@students.ku.ac.ke>
2022-09-09 07:58:59 +0300

    job feedback component

--- src/components/Pages/Hire.js
+++ src/components/Pages/Hire.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import "../styles/Hire.css"
 
 function Hire() {
   return (
-      <div>
-        Hire a Dev
+      <div className="hire-container">
+        <h3 className="hire-header">Thank you</h3>
+        <img className="hire-img" src="./check.png" alt="tick" />
+        <h3 className="hire-header">Your request has been received...</h3>
       </div>
   );
 }
